884ca441c027da9f3c6769c905509b25ced28446
easility <prashant@easility.com>
2013-03-15 20:07:55 +0530

    Adding the project files

---
+++ app/com/alvazan/orm/models/test/Account.java
@@ -0,0 +1,126 @@
+package com.alvazan.orm.models.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.alvazan.orm.api.base.CursorToMany;
+import com.alvazan.orm.api.base.CursorToManyImpl;
+import com.alvazan.orm.api.base.NoSqlEntityManager;
+import com.alvazan.orm.api.base.Query;
+import com.alvazan.orm.api.base.anno.NoSqlEntity;
+import com.alvazan.orm.api.base.anno.NoSqlId;
+import com.alvazan.orm.api.base.anno.NoSqlIndexed;
+import com.alvazan.orm.api.base.anno.NoSqlManyToMany;
+import com.alvazan.orm.api.base.anno.NoSqlOneToMany;
+import com.alvazan.orm.api.base.anno.NoSqlQueries;
+import com.alvazan.orm.api.base.anno.NoSqlQuery;
+
+@NoSqlEntity
+@NoSqlQueries({
+	@NoSqlQuery(name="findBetween", query="select b from Account as b where b.users >= :begin and (b.name = :name or b.name = :name) and b.users < :end"),
+	@NoSqlQuery(name="findAll", query="select *  from Account as d"),
+	@NoSqlQuery(name="findAnd", query="select *  FROM Account as a WHERE a.name=:name and a.isActive=:active"),
+	@NoSqlQuery(name="findOr", query="select *  FROM Account as a WHERE a.name=:name or a.isActive=:active")
+})
+public class Account extends AccountSuper{
+
+	@NoSqlId
+	private String id;
+	
+	@NoSqlIndexed
+	private String name;
+	
+	private String onemoreString;
+	
+	public String getOnemoreString() {
+		return onemoreString;
+	}
+
+	public void setOnemoreString(String onemoreString) {
+		this.onemoreString = onemoreString;
+	}
+
+
+
+
+
+	@NoSqlIndexed
+	private Float users;
+
+	//@Transient
+	@NoSqlOneToMany
+	private List<Activity> activities = new ArrayList<Activity>();
+	
+	@NoSqlManyToMany
+	private CursorToMany<Activity> activitiesCursor = new CursorToManyImpl<Activity>();
+	
+	public Account() {}
+	
+	public Account(String id ) {
+		this.id = id;
+	}
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Float getUsers() {
+		return users;
+	}
+
+	public void setUsers(Float users) {
+		this.users = users;
+	}
+	
+	public static List<Account> findBetween(NoSqlEntityManager mgr, float begin, float to) {
+		Query<Account> query = mgr.createNamedQuery(Account.class, "findBetween");
+		query.setParameter("begin", begin);
+		query.setParameter("end", to);
+		return query.getResultList(0, null);
+	}
+	public static List<Account> findAll(NoSqlEntityManager mgr) {
+		Query<Account> query = mgr.createNamedQuery(Account.class, "findAll");
+		return query.getResultList(0, null);
+	}
+	public static List<Account> findAnd(NoSqlEntityManager mgr, String name, Boolean active) {
+		Query<Account> query = mgr.createNamedQuery(Account.class, "findAnd");
+		query.setParameter("name", name);
+		query.setParameter("active", active);
+		return query.getResultList(0, null);
+	}
+
+	public static List<Account> findOr(NoSqlEntityManager mgr, String name,
+			boolean active) {
+		Query<Account> query = mgr.createNamedQuery(Account.class, "findOr");
+		query.setParameter("name", name);
+		query.setParameter("active", active);
+		return query.getResultList(0, null);
+	}
+
+	public List<Activity> getActivities() {
+		return activities;
+	}
+
+	
+	public CursorToMany<Activity> getActivitiesCursor() {
+		return activitiesCursor;
+	}
+
+	public void addActivity(Activity act1) {
+		activities.add(act1);
+		activitiesCursor.addElement(act1);
+	}
+	
+}
--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,123 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import java.util.ArrayList;
+import java.util.Collection;
 
-import views.html.*;
+import java.util.List;
+
+
+
+import play.Play;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
+
+import com.alvazan.orm.api.base.NoSqlEntityManager;
+import com.alvazan.orm.api.z3api.NoSqlTypedSession;
+import com.alvazan.orm.api.z5api.IndexPoint;
+import com.alvazan.orm.api.z8spi.iter.Cursor;
+import com.alvazan.orm.api.z8spi.meta.DboColumnMeta;
+import com.alvazan.orm.api.z8spi.meta.DboDatabaseMeta;
+import com.alvazan.orm.api.z8spi.meta.DboTableMeta;
+import com.alvazan.orm.api.z8spi.meta.TypedRow;
+import com.alvazan.orm.models.test.Account;
+import com.alvazan.play.NoSql;
 
 public class Application extends Controller {
-  
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
-  
+
+	static Cursor<List<TypedRow>> rowsIter;
+	static int batchsize = 50;
+
+	public static Result index() {
+		return ok(index.render("Test String"));
+	}
+
+	public static Result allTables() {
+		//String keyspace = Play.application().configuration().getString("nosql.cassandra.keyspace");
+		NoSqlEntityManager mgr = NoSql.em();
+		DboDatabaseMeta database = mgr.find(DboDatabaseMeta.class,
+				DboDatabaseMeta.META_DB_ROWKEY);
+		Collection<DboTableMeta> allColumnFamilies = database.getAllTables();
+		ArrayList<String> tableList = new ArrayList<String>();
+		for (DboTableMeta tableMeta : allColumnFamilies) {
+			System.out.println("Adding table :" + tableMeta.getColumnFamily() + " in list");
+			tableList.add(tableMeta.getColumnFamily());
+		}
+		//return ok(views.html.allTables.render(tableList));
+		System.out.println("SUCESSFUL TILL HERE");
+		return TODO;
+	}
+
+	public static Result detailResult(String columnFamilyName) {
+		NoSqlEntityManager mgr = NoSql.em();
+		DboDatabaseMeta database = mgr.find(DboDatabaseMeta.class,
+				DboDatabaseMeta.META_DB_ROWKEY);
+		Collection<DboTableMeta> allTables = database.getAllTables();
+		ArrayList<String> columnList = new ArrayList<String>();
+		ArrayList<String> indexList = new ArrayList<String>();
+		ArrayList<String> showList = new ArrayList<String>();
+		for (DboTableMeta tableMeta : allTables) {
+			if (tableMeta.getColumnFamily().equals(columnFamilyName)) {
+				Collection<DboColumnMeta> allColumns = tableMeta
+						.getAllColumns();
+				Collection<DboColumnMeta> allIndexColumn = tableMeta
+						.getIndexedColumns();
+				for (DboColumnMeta column : allColumns)
+					columnList.add(column.getColumnName());
+				for (DboColumnMeta index : allIndexColumn)
+					indexList.add(index.getColumnName());
+				for (DboColumnMeta column : allColumns) {
+					if (!indexList.contains(column.getColumnName()))
+						showList.add(column.getColumnName());
+				}
+				break;
+			}
+		}
+		return TODO;
+		//render(indexList, columnFamilyName, showList);
+	}
+
+	public static void processIndex(String columnFamilyName, String index) {
+		NoSqlEntityManager mgr = NoSql.em();
+		NoSqlTypedSession s = mgr.getTypedSession();
+		ArrayList<String> columnList = new ArrayList<String>();
+		Cursor<IndexPoint> indexView = s.indexView(columnFamilyName, index,
+				null, null);
+		int count = 0;
+		int first = 50;
+		while (indexView.next() && count < first) {
+			IndexPoint current = indexView.getCurrent();
+			String indVal = current.getIndexedValueAsString();
+			columnList.add(indVal);
+			count++;
+		}
+		//render(columnList, columnFamilyName, index, first, count);
+	}
+	
+	public static Result get() {
+		NoSqlEntityManager mgr = NoSql.em();
+		TypedRow row = mgr.getTypedSession().find("Account", "acc1");
+		if (row !=null ) {
+			System.out.println("row.getRowKeyString()" + row.getRowKeyString());
+			System.out.println("row.getRowKeyString()" + row.getColumnsAsColl().toString());
+		}		
+        System.out.println("GET SUCESSFUL TILL HERE");
+		return TODO;
+		
+	}
+	
+	public static Result save() {
+		NoSqlEntityManager mgr = NoSql.em();
+        Account acc1e = new Account();
+        acc1e.setId("acc1");
+        acc1e.setIsActive(false);
+        acc1e.setSomeField(34);
+		mgr.put(acc1e);
+		mgr.flush();
+        System.out.println("SUCESSFUL SAVE TILL HERE");
+		return TODO;
+	}
+
+
 }
